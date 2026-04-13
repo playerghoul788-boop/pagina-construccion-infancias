2da3d502bc79674f94cb326121705ceceb56720d
playerghoul788-boop <playerghoul788@gmail.com>
2026-04-13 15:56:08 -0500

    Update .js

--- .js
+++ .js
@@ -1,1 +1,19 @@
+<script>
+let escala = 1;
+const img = document.getElementById("imagenZoom");
 
+img.addEventListener("wheel", function(e) {
+    e.preventDefault();
+
+    if (e.deltaY < 0) {
+        escala += 0.1; // acercar
+    } else {
+        escala -= 0.1; // alejar
+    }
+
+    if (escala < 1) escala = 1;
+    if (escala > 5) escala = 5;
+
+    img.style.transform = "scale(" + escala + ")";
+});
+</script>
